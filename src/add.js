0f63d214c08b376563b80b84395a72dd8c2a33a3
Oleksandr Bakai <saniabakay@gmail.com>
2025-06-21 18:41:23 +0200

    no bonus

--- src/add.js
+++ src/add.js
@@ -1,7 +1,11 @@
 function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
+    if (numOne == undefined || 
+        numTwo == undefined ||
+         typeof numOne!="number"||
+         typeof numTwo !="number") {
         return undefined;
     }
-
-    return numOne + numTwo;
+    
+       return numOne + numTwo;
 }
+add(2,2);
--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,15 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      //my test
+      it("The function should return undefined if any of the two arguments is not a number.",()=>{
+        expect(add(undefined,4)).toEqual(undefined);
+        expect(add(5, undefined)).toEqual(undefined);
+        expect(add(undefined,undefined)).toEqual(undefined);
+      });
     });
   });
+
+ 
+
   
